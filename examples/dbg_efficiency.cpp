e1b7051c52c74171c114a4c00a7529dab5a3a092
winapiadmin <asdfsddf1@outlook.com>
2025-11-22 10:52:52 +0700

    removed examples and modernize with forward declarations (bonus: make more incremental update)

--- examples/dbg_efficiency.cpp
+++
@@ -1,6 +0,0 @@
-#include "position.h"
-int main() {
-    std::cout << sizeof(chess::HistoryEntry<chess::PolyglotPiece>) << " " << sizeof(chess::HistoryEntry<chess::EnginePiece>) << '\n';
-    std::cout << "8192 entries/board (impl) "<< (8192*sizeof(chess::HistoryEntry<chess::PolyglotPiece>)) << " " << (8192*sizeof(chess::HistoryEntry<chess::EnginePiece>)) << '\n';
-    return 0;
-}
---
+++ fwd_decl.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <cstdint>
+#include <type_traits>
+namespace chess {
+enum Color : uint8_t;
+enum PieceType : std::int8_t;
+
+template <typename T, typename = void> struct is_piece_enum : std::false_type {};
+
+template <typename T> struct is_piece_enum<T, std::void_t<decltype(T::PIECE_NB)>> : std::true_type {};
+enum CastlingRights : int8_t;
+enum Square : int8_t;
+enum Direction : int8_t;
+enum MoveType : uint16_t;
+enum File : int8_t;
+enum Rank : int8_t;
+class Move;
+template <typename T, typename> class _Position;
+using Bitboard=uint64_t;
+using Key = uint64_t;
+// bonus: define the piece enums here
+enum class PolyglotPiece : uint8_t;
+enum class EnginePiece : uint8_t;
+enum class ContiguousMappingPiece : uint8_t;
+} // namespace chess
