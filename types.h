6558d7e68bff82903b5e06fe19183801210824bf
winapiadmin <138602885+winapiadmin@users.noreply.github.com>
2026-02-25 11:33:24 +0700

    Update types.h

--- types.h
+++ types.h
@@ -62,7 +62,7 @@ namespace chess {
 using Bitboard = uint64_t;
 using Key = uint64_t;
 // clang-format off
-enum Square {
+enum Square : int8_t {
     SQ_A1, SQ_B1, SQ_C1, SQ_D1, SQ_E1, SQ_F1, SQ_G1, SQ_H1,
     SQ_A2, SQ_B2, SQ_C2, SQ_D2, SQ_E2, SQ_F2, SQ_G2, SQ_H2,
     SQ_A3, SQ_B3, SQ_C3, SQ_D3, SQ_E3, SQ_F3, SQ_G3, SQ_H3,
