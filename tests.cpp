b457d385f942858bb197de5eaae4ede5b3604576
winapiadmin <138602885+winapiadmin@users.noreply.github.com>
2026-03-29 23:37:57 +0700

    Add tests for move, null move, and undo move

--- tests.cpp
+++ tests.cpp
@@ -404,6 +404,17 @@ TEST_CASE("Experienced bugs in this repo") {
     }
     {
         Position p;
+        p.doMove<false>(Move(SQ_F7, SQ_F5));
+        REQUIRE(p.zobrist() == p.hash());
+        p.doNullMove();
+        REQUIRE(p.zobrist() == p.hash());
+        p.undoMove();
+        REQUIRE(p.zobrist() == p.hash());
+        p.undoMove();
+        REQUIRE(p.zobrist() == p.hash());
+    }
+    {
+        Position p;
         REQUIRE(p.getCastlingPath(WHITE, true) == 0x60);
         REQUIRE(p.getCastlingPath(WHITE, false) == 0xe);
         REQUIRE(p.getCastlingPath(BLACK, true) == 0x6000000000000000ULL);
